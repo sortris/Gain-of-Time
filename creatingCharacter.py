c37144f97cc6b598d7c58c86b6f8b0522ff36bf8
Tobiasz Siemiński <sortris@gmail.com>
2011-08-24 22:54:11 +0200

    hehehe

--- creatingCharacter.py
+++ creatingCharacter.py
@@ -4,7 +4,7 @@
 # creation date: 21.08.2011
 
 def createCharacter():
-	name = raw_input("Podaj imię postaci:\n")
+	name = raw_input("Podaj simię postaci:\n")
 	file = open(name+".txt", 'w+')
 	file.write(name)
 	file.close()
